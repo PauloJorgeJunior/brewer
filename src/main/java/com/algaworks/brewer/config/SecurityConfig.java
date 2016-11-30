35cd996e4e17d334bbc9eeb77cd2aceed4938ef2
Paulo Jorge Junior <p4ulojorge@gmail.com>
2016-11-29 23:04:34 -0400

    Aula 19.3

--- src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
 	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/layout/**")
+			.antMatchers("/images/**");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -23,6 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
+				.loginPage("/login")
+				.permitAll()
 				.and()
 			.csrf().disable();
 	}
